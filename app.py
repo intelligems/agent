2139012c0b06308ef4d46981d3abcd0d48604e89
TDaglis <thanasis@sourcelair.com>
2016-10-12 18:33:33 +0300

    Simplify code

--- app.py
+++ app.py
@@ -18,22 +18,16 @@ def check_auth(username, password):
     return valid_uname and valid_pass
 
 
-def authenticate():
-    """Sends a 401 response that enables basic auth
-    """
-    return Response(
-        'Could not verify your access level for that URL.\n'
-        'You have to login with proper credentials', 401,
-        {'WWW-Authenticate': 'Basic realm="Login Required"'}
-    )
-
-
 def requires_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         auth = request.authorization
         if not auth or not check_auth(auth.username, auth.password):
-            return authenticate()
+            return Response(
+                'Could not verify your access level for that URL.\n'
+                'You have to login with proper credentials', 401,
+                {'WWW-Authenticate': 'Basic realm="Login Required"'}
+            )
         return f(*args, **kwargs)
     return decorated
 
